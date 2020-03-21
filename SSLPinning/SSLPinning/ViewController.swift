d504e85ff366306840bf9a2746b7ef598847f46c
anujrai <anuj.rai2489@gmail.com>
2020-03-21 16:11:02 +0530

    Removed commented code

--- SSLPinning/SSLPinning/ViewController.swift
+++ SSLPinning/SSLPinning/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let url = URL(string: "https://www.google.co.uk") else { return }
-        //ServiceManager().callAPI(withURL: url, isCertificatePinning: false)
         ServiceManager().callAPI(withURL: url, isCertificatePinning: false) { (message) in
             let alert = UIAlertController(title: "SSLPinning", message: message, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
